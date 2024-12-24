23c43a34c7240554addec73fdda393f8fd0df34f
Artat345 <Palla3palla@gmail.com>
2024-12-24 23:06:31 +0500

    Add files via upload

---
+++ 15/15/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace GreetingApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GreetButton_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+
+            // Формируем приветствие
+            string greeting = $"Привет, {firstName} {lastName}!";
+            GreetingTextBlock.Text = greeting;
+        }
+    }
+}
