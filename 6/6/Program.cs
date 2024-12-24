d77e8cbe24d5f13d8722beab1fcc3aecd90621be
Artat345 <Palla3palla@gmail.com>
2024-12-24 22:48:38 +0500

    Add files via upload

---
+++ 6/6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+public class FileMerger
+{
+    public static void MergeFiles(string directoryPath, string outputFilePath)
+    {
+        using (StreamWriter writer = new StreamWriter(outputFilePath))
+        {
+            // Получаем все текстовые файлы в указанной директории
+            string[] filePaths = Directory.GetFiles(directoryPath, "*.txt");
+
+            foreach (string filePath in filePaths)
+            {
+                try
+                {
+                    // Читаем содержимое файла и записываем в выходной файл
+                    string content = File.ReadAllText(filePath);
+                    writer.WriteLine(content);
+                    Console.WriteLine($"Содержимое файла {filePath} добавлено в {outputFilePath}.");
+                }
+                catch (IOException ex)
+                {
+                    // Обработка ошибок ввода-вывода
+                    Console.WriteLine($"Ошибка ввода-вывода при обработке файла {filePath}: {ex.Message}. Пропускаем.");
+                }
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к директории с текстовыми файлами:");
+        string directoryPath = Console.ReadLine();
+
+        Console.WriteLine("Введите путь к выходному файлу:");
+        string outputFilePath = Console.ReadLine();
+
+        MergeFiles(directoryPath, outputFilePath);
+    }
+}
